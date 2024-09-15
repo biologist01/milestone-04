9fb4ef1dea884d125933432bda0139b9a92e901b
biologist01 <159536284+biologist01@users.noreply.github.com>
2024-09-15 13:29:13 +0500

    Add files via upload

---
+++ EDITABLE-SCRIPT/index.ts
@@ -0,0 +1,73 @@
+let gName = document.querySelector('.gName') as HTMLHeadingElement;
+gName.addEventListener('input', () => {
+    if(gName.innerHTML.length >= 15){
+        gName.innerHTML = gName.innerHTML.slice(0, 15)
+    }
+})
+
+let number = document.querySelector('.number') as HTMLHeadingElement;
+number.addEventListener('input', () => {
+    if(number.innerHTML.length > 13){
+        number.innerHTML = number.innerHTML.slice(0, 13)
+    } else if(number.innerHTML.length < 13){
+        number.style.border = '2px solid red';
+    } else {
+        number.style.border = 'none';
+    }
+})
+
+//*! -------------------------- for education section ------------------------- */
+
+let st_ul = document.querySelector('.education .styled-ul') as HTMLUListElement;
+let st_div = document.querySelector('.education-container') as HTMLDivElement ;
+
+st_ul.addEventListener("click",() =>{
+    let st_li = document.querySelectorAll('.education .styled-ul li') as NodeListOf<HTMLLIElement>;
+let combined= Array.from(st_li).map(li => li.innerHTML).join(",");
+console.log(combined , st_li);
+st_ul.style.display = "none"
+st_div.classList.remove("disappear")
+st_div.innerHTML = combined;
+
+st_div.addEventListener("focusout", ()=>{
+   let sepArr = st_div.innerText.split(",");
+   console.log(sepArr , st_div.innerText)
+   st_ul.innerHTML = ""
+   sepArr.forEach((edu) => {
+       st_ul.innerHTML = st_ul.innerHTML + `<li class="styled-list">${edu}</li>`  
+    
+   })
+   st_ul.style.display = "flex"
+   st_div.classList.add("disappear")
+})
+
+})
+
+//*! -------------------------- for skill section ------------------------- */
+
+let skill_ul = document.querySelector('.skills .styled-ul') as HTMLUListElement;
+let skill_div = document.querySelector('.skill-container') as HTMLDivElement ;
+
+
+skill_ul.addEventListener("click",() =>{
+    let skill_li = document.querySelectorAll('.skills .styled-ul li') as NodeListOf<HTMLLIElement>;
+let combined  = Array.from(skill_li).map(li => li.innerHTML).join(",");
+skill_ul.style.display = "none"
+skill_div.classList.remove("disappear")
+skill_div.innerHTML = combined;
+
+skill_div.addEventListener("focusout", ()=>{
+   let sepArr = skill_div.innerText.split(",");
+   console.log(sepArr , skill_div.innerText)
+   skill_ul.innerHTML = ""
+   sepArr.forEach((edu) => {
+       skill_ul.innerHTML = skill_ul.innerHTML + `<li class="styled-list">${edu}</li>`  
+    
+   })
+   skill_ul.style.display = "flex"
+   skill_div.classList.add("disappear")
+})
+
+})
+
+//*! ----------------------------- for experience ----------------------------- */
